51b1939d16ba7504da7e78bd2c533c2910be5eaa
Inho Oh <inho.oh@sk.com>
2019-10-18 17:15:06 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,10 +4,12 @@ echo "Start..."
 echo "$GITHUB_WORKSPACE"
 echo "$GITHUB_REF"
 echo "$GITHUB_SHA"
+pwd
 
-for sha1 in $(git rev-list master..HEAD); do
+
+for sha1 in $(git rev-list HEAD); do
     echo "Commit id $sha1"
-    git show --format=email $sha1 | checkpatch.pl -q --no-tree -
+    #git show --format=email $sha1 | checkpatch.pl -q --no-tree -
 done
 
 echo "Done"
