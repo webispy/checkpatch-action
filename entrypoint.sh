364b1db90600cba9da15847635c0618bb7da3dc0
Inho Oh <inho.oh@sk.com>
2019-10-21 11:24:32 +0900

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
 
 echo "Start..."
+echo "Workflow: $GITHUB_WORKFLOW"
+echo "Action: $GITHUB_ACTION"
+echo "Actor: $GITHUB_ACTOR"
+echo "Repository: $GITHUIB_REPOSITORY"
+echo "Event-name: $GITHUB_EVENT_NAME"
+echo "Event-path: $GITHUB_EVENT_PATH"
 echo "Workspace: $GITHUB_WORKSPACE"
+echo "SHA: $GITHUB_SHA"
 echo "REF: $GITHUB_REF"
 echo "HEAD-REF: $GITHUB_HEAD_REF"
 echo "BASE-REF: $GITHUB_BASE_REF"
-echo "SHA: $GITHUB_SHA"
 echo "TOKEN: $GITHUB_TOKEN"
 pwd
 
@@ -21,5 +27,16 @@ for sha1 in $(git rev-list origin/$GITHUB_BASE_REF..origin/$GITHUB_HEAD_REF); do
     echo "Commit id $sha1"
     /review.sh $sha1
 done
+
+$PR=${GITHUB_REFS#"refs/"}
+$PRNUM=${PR%"/merge"}
+
+#$URL=https://api.github.com/repos/${GITHUB_REPOSITORY}/pulls/${PRNUM}/comments
+$URL=https://api.github.com/repos/${GITHUB_REPOSITORY}/issues/${PRNUM}/comments
+echo $URL
+
+curl -s -H "Authorization: token ${GITHUB_TOKEN} \
+    -X POST -d '{ "body": "hi" }' \
+    $URL
 
 echo "Done"
